4b8701c82c17fdab92b8ae98ee32d2bc4e17c8af
GONBEEE_project_myhouse0 <gonbeee.project@gmail.com>
2018-07-06 21:46:28 +0900

    mi-soさん実装

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class InputManager : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject Map;
+
+    private bool mapEnable=true;
+
+
+
+    void Start()
+    {
+        InteractionManager.InteractionSourcePressed += SourcePressed;
+    }
+    void SourcePressed(InteractionSourcePressedEventArgs state)
+    {
+        mapEnable = !mapEnable;
+
+        Map.SetActive(mapEnable);
+
+        // Press
+    }
+
+}
